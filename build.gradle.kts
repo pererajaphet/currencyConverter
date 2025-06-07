0a95d9596f223ad27d0d1d56edcc89fa94ab6093
pererajaphet <pererajaphet@gmail.com>
2025-06-07 09:13:43 +0200

    first commit - set build.gradle config

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins{
+    java
+    application
+}
+
+application {
+    mainClass.set("Main")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+    implementation("org.apache.logging.log4j:log4j-api:2.20.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.20.0")
+
+
+}
+
+tasks.test{
+    useJUnitPlatform()
+}
